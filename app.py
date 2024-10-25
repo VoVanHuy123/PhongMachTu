d86143b2bdc1ad9e8e04716e3372030c79314e42
hungtun <113239877+hungtun@users.noreply.github.com>
2024-10-25 13:32:24 +0700

    h

--- app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, redirect, url_for,render_template
-
-app = Flask(__name__)
-@app.route("/")
-def hello_user():
-    return render_template('index.html',content="hung",
-                           cars = ["Vinfast", "BMW", "Mercedes"])
-if __name__ == '__main__':   
-    app.run(debug=True)
