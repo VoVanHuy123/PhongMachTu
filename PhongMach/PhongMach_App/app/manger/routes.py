0484bfb1855d666d2b70e9980c45724913f13ff5
VoVanHuy123 <vovanhuy2k4@gmail.com>
2024-11-24 21:55:19 +0700

    delete

--- PhongMach/PhongMach_App/app/manger/routes.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template
-
-manager = Blueprint('manager', __name__, url_prefix='/manager')
-
-@manager.route('/')
-def manager_run():
-    return render_template('manger.html')
